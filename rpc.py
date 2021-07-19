f5107f3b805c9cfb3ddadcd2f03ed254984300c9
demirose41 <demirose41@gmail.com>
2021-07-19 06:29:28 -0700

    Game can now play through an enitre set of player specified rounds of rock paper scissors for a single loop

--- rpc.py
+++ rpc.py
@@ -82,16 +82,23 @@ def compare (x,y):
         return "p1"
     if x == "paper" and y == "rock" :
         return "p1"
+    if x == y :
+        return "tie"
     else :
         return "p2"
 
 
-        
+def divider () :
+    for num in range (0,24,2) :
+        print("####################################\n")
+        print("# # # # # # # # # # # # # # # # # #\n")
+
 
 
 score_p1 = 0
 score_p2 = 0
 round = 0 
+live_score = {score_p1,score_p2}
 
 
 print("who will be first")
@@ -99,17 +106,42 @@ p1_name = input()
 print("who will be second")
 p2_name = input()
 print("how many rounds?")
-win_condition = int(input()) / 2 
+win_condition = math.ceil( int(input()) / 2 )
 print(f"{win_condition} win con")
 while score_p2 < win_condition  or score_p1 < win_condition:
-    print(score_p2)
-    print(score_p1)
-    print(round)
-    print(win_condition)
-    input()
-    score_p2 +=1 
-    score_p1 += 1
-    round += 1
+    print(f"SCORE \n {score_p1} - {score_p2}")
+    print(f"Round {round}")
+    print("####################################\n") 
+    print(f"{p1_name}! WHAT IS YOUR CHOICE")
+    print("\n####################################\n\n")
+    p1_input = input()
+    divider()
+    print("####################################\n")
+    print(f"{p2_name}! WHAT IS YOUR CHOICE")
+    print("\n####################################\n\n")
+    p2_input = input()
+
+    outcome = compare(p1_input,p2_input)
+
+    if outcome == "p1" :
+        score_p1 += 1 
+        round += 1
+        print(f"{p1_name} is the winner of this round")
+        divider()
+    elif outcome == "p2" :
+            score_p2 += 1
+            round += 1
+            divider()
+            print(f"{p2_name} is the winner of this round")
+            input("continue")
+            divider()
+    else :
+        divider()
+        print("Tie ")
+        input("continue")
+        divider()
+            
+
 
 print("out")
 
